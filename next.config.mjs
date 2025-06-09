a511ff5ef228381fb92026777ba6f8db6530cf69
Ahmad Dangana <danganaahmed5@gmail.com>
2025-06-10 00:09:10 +0100

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["raw.githubusercontent.com", "https://isthiscoinascam.com", "https://www.cryptologos.cc",],
   },
+  // Disable SWC minifier to fix RainbowKit build issues
+  swcMinify: false,
+
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
