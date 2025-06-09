01f286df36fa02e336635ac8f43517e5aa4f3f15
Ahmad Dangana <danganaahmed5@gmail.com>
2025-06-09 09:06:10 +0100

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ["raw.githubusercontent.com", "https://isthiscoinascam.com", "https://www.cryptologos.cc",],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer = config.optimization.minimizer.map(minimizer => {
+        if (minimizer.constructor.name === 'TerserPlugin') {
+          minimizer.options.exclude = /HeartbeatWorker\.js$/;
+        }
+        return minimizer;
+      });
+    }
+  }
 };
 
 export default nextConfig;
