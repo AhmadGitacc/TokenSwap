5c616f49d5e38e9112d3193bd1e61826ec783014
Ahmad Dangana <danganaahmed5@gmail.com>
2025-06-09 14:20:46 +0100

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ const nextConfig = {
   images: {
     domains: ["raw.githubusercontent.com", "https://isthiscoinascam.com", "https://www.cryptologos.cc",],
   },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+
+    // Handle worker files
+    config.module.rules.push({
+      test: /\.worker\.js$/,
+      use: { loader: 'worker-loader' },
+    });
+
+    return config;
+  },
+  transpilePackages: ['@rainbow-me/rainbowkit'],
 };
 
 export default nextConfig;
