b333e5d87a4f3f9b575e7fd24262f0910ef7ca57
Ahmad Dangana <danganaahmed5@gmail.com>
2025-06-09 14:05:14 +0100

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     domains: ["raw.githubusercontent.com", "https://isthiscoinascam.com", "https://www.cryptologos.cc",],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
