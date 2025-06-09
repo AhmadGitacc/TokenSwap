4c864911097e662114016e52dad558c532110f8b
Ahmad Dangana <danganaahmed5@gmail.com>
2025-06-09 09:25:43 +0100

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ["raw.githubusercontent.com", "https://isthiscoinascam.com", "https://www.cryptologos.cc",],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /HeartbeatWorker\.js$/,
-      use: {
-        loader: 'worker-loader',
-        options: {
-          inline: true,
-          publicPath: '/_next/static/workers/',
-        },
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
